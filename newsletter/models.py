aa88a991103c06109f851ba3488d3a5d1663cb9c
Marcin Nowak <marcin.j.nowak@gmail.com>
2010-04-14 21:52:38 +0200

    fixed is_subscribed, added manager, cleaned subscription model fields

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,48 +1,52 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
-import datetime
+import warnings
+
+class SubscriptionManager(models.Manager):
+    """
+    Newsletter subscription manager
+    """
+    def is_email_subscribed(self, email):
+        try:
+            return self.get_query_set().get(email=email).subscribed
+        except Subscription.DoesNotExist:
+            return False
+
 
 class SubscriptionBase(models.Model):
-    '''
+    """
     A newsletter subscription base.
-    
-    '''
+    """
 
     subscribed = models.BooleanField(_('subscribed'), default=True)
     email = models.EmailField(_('email'), unique=True)
-    created_on = models.DateField(_("created on"), blank=True)
-    updated_on = models.DateField(_("updated on"), blank=True)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    updated_on = models.DateTimeField(_("updated on"), auto_now_add=True,
+            auto_now=True)
+
+    objects = SubscriptionManager()
     
     class Meta:
         abstract = True
     
     @classmethod
     def is_subscribed(cls, email):
-        '''
+        """
         Concept inspired by Satchmo. Thanks guys!
-        
-        '''
-        try:
-            return cls.objects.get(email=email).subscribed
-        except cls.DoestNotExist, e:
-            return False
-         
+        But this is a bad idea. Use manager to check subscription:
+        >>> Subscription.objects.is_email_subscribed(email)
+        """
+        warnings.warn('will be removed in future', DeprecationWarning)
+        return cls.objects.is_email_subscribed(email)
     
     def __unicode__(self):
         return u'%s' % (self.email)
         
-    def save(self, *args, **kwargs):
-        self.updated_on = datetime.date.today()
-        if not self.created_on:
-            self.created_on = datetime.date.today()
-        super(SubscriptionBase,self).save(*args, **kwargs)
 
 class Subscription(SubscriptionBase):
-    '''
+    """
     Generic subscription
-    
-    '''
+    """
+    pass
         
-    def save(self, *args, **kwargs):
-        super(Subscription,self).save()
